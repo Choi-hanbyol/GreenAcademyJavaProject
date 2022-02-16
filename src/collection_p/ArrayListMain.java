cde584ff89c283aa1eff0ab43b2c251b394d65d2
choi-hanbyol <chbyol@naver.com>
2022-02-16 17:20:54 +0900

    2022/02/16 ArrayList 예제

---
+++ src/collection_p/ArrayListMain.java
@@ -0,0 +1,63 @@
+package collection_p;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListMain {
+
+	public static void main(String[] args) {
+		
+		List arr1 = new ArrayList();
+		
+		arr1.add(10);
+		arr1.add(20);
+		arr1.add("아기상어");
+		arr1.add(123.456);
+		arr1.add(true);
+		arr1.add(new int [] {11,22,33});
+		arr1.add(null);
+		arr1.add(10);
+		arr1.add(30);
+		arr1.add(40);
+		arr1.add(20);
+		arr1.add(10);
+		arr1.add(20);
+		arr1.add(70);
+		
+		System.out.println(arr1);
+		System.out.println(arr1.size());
+		System.out.println(arr1.get(2));
+		System.out.println("set() 명령어");
+		System.out.println("arr1.set(4, \"엄마상어\")");
+		arr1.set(4, "엄마상어");
+		System.out.println(arr1);
+		
+		System.out.println("============================");
+		System.out.println("add() 명령어");
+		System.out.println("arr1.add(3, \"장수풍뎅이\")");
+		arr1.add(3,"장수풍뎅이");
+		System.out.println(arr1);
+		
+		System.out.println("<<<<<<<<remove명령어>>>>>>>>");
+		arr1.remove(6);
+		System.out.println(arr1);
+		
+		System.out.println("============================");
+		arr1.remove("아기상어");
+		System.out.println(arr1);
+		
+		System.out.println("============================");
+		arr1.remove((Object)20);
+		System.out.println(arr1);
+		
+		System.out.println("============================");
+		System.out.println(arr1.contains(30));
+		System.out.println(arr1.contains(100));
+		System.out.println(arr1.indexOf(20));
+		System.out.println(arr1.indexOf(100));
+		System.out.println(arr1.lastIndexOf(20));
+		
+
+	}
+
+}
